7607a368422370d041216caa47227162a0579193
Mark van der Loo <mark.vanderloo@gmail.com>
2017-01-16 22:00:30 +0100

    EM/EMB now use do_by (and keep input data attributes)

--- pkg/R/em.R
+++ pkg/R/em.R
@@ -5,25 +5,26 @@
 #' @export
 impute_em <- function(dat, formula, p2s=0,...){
   if ( not_installed("Amelia") ) return(dat)
-  groups <- groups(dat,formula)
-  formula <- remove_groups(formula)
-  prd <- get_predictors(formula, dat)
+  grp <- groups(dat,formula)
+  frm <- remove_groups(formula)
+  prd <- get_predictors(frm, dat)
   imp <- get_imputed(formula, dat)
   imp <- unique(c(imp, prd))
-
-  d_imp <- iunsplit(lapply(isplit(dat[imp],dat[groups]), function(d){
+  
+  imp_work <- function(dd){
+    d <- dd[imp]
     # We need this ugly escape since Amelia sends errors when passed a
     # complete dataset.
-    if (!anyNA(d)) return(d)
+    if (!anyNA(d)) return(dd)
     # Ok, let's get to work
     out <- tryCatch({Amelia::amelia(d, m=1, p2s=p2s, boot.type="none", ...)}
-        , error = function(e){
-          warnf("Amelia::amelia stopped with message\n %s\n Returning original data"
-                , e$message)
-          FALSE
+      , error = function(e){
+        warnf("Amelia::amelia stopped with message\n %s\n Returning original data"
+              , e$message)
+        FALSE
     }) # end tryCatch
     # if Amelia stopped, return data untouched
-    if (identical(out,FALSE)) return(d)
+    if (identical(out,FALSE)) return(dd)
     
     # extract parameters (computed on z-transformed columns)
     mu_sc <- out$mu
@@ -34,8 +35,8 @@ impute_em <- function(dat, formula, p2s=0,...){
     mu_x <- colMeans(x,na.rm=TRUE)
     sd_x <- apply(x,2,sd,na.rm=TRUE)
     x_sc <- scale(x,center=mu_x, scale=sd_x)
-
-    # Impute scaled values. For derivation of imputation equation see e..g
+    
+    # Impute scaled values. For derivation of imputation equation see e.g.
     # http://fourier.eng.hmc.edu/e161/lectures/gaussianprocess/node7.html
     a <- apply(x_sc,1,function(x_){
       i_miss <- is.na(x_)
@@ -48,13 +49,13 @@ impute_em <- function(dat, formula, p2s=0,...){
       x_[i_miss] <- mu_miss + Smo%*%solve(Soo,(x_obs - mu_obs))
       x_
     })
-    as.data.frame(unscale(t(a),mu=mu_x,sd=sd_x))
-  }),dat[groups]) # end iunsplit
-  dat[imp] <- d_imp 
-  dat
+    dd[imp] <- as.data.frame(unscale(t(a),mu=mu_x,sd=sd_x))
+    dd
+  }
+  
+  do_by(dat=dat, groups=grp, .fun = imp_work)  
+
 }
-
-
 
 unscale <- function(x,mu,sd){
   for ( i in seq_len(ncol(x)) ){
@@ -73,23 +74,29 @@ unscale <- function(x,mu,sd){
 #' @export
 impute_emb <- function(dat, formula, p2s=0, ...){
   if ( not_installed("Amelia") ) return(dat)
-  groups <- groups(dat, formula)
-  formula <- remove_groups(formula)
-  prd <- get_predictors(formula, dat)
+  grp <- groups(dat, formula)
+  frm <- remove_groups(formula)
+  prd <- get_predictors(frm, dat)
   imp <- get_imputed(formula, dat)
   imp <- unique(c(imp, prd))
- 
-  d_imp <- iunsplit(lapply(isplit(dat[imp],dat[groups]), function(d){
-    if (!anyNA(d)) return(d)
-    tryCatch(Amelia::amelia(d, m=1, p2s=p2s, ...)$imputations[[1]]
+
+  imp_work <- function(dd){
+    d <- dd[imp]
+    # workaround since amelia errors when no missings present
+    if (!anyNA(d)) return(dd)
+    # actual work
+    out <- tryCatch(Amelia::amelia(d, m=1, p2s=p2s, ...)$imputations[[1]]
       , error = function(e){
        warnf("Amelia::amelia stopped with message\n %s\n Returning original data"
-             , e$message)
-       d
-    })  
-    
-  }), dat[groups]) # end iunsplit
+          , e$message
+        ) # end tryCatch
+      FALSE
+    })
+    if (identical(out,FALSE)) return(dd)
+    dd[imp] <- out
+    dd
+  }
   
-  dat[imp] <- d_imp
-  dat
+  do_by(dat, grp, imp_work)
+
 }
