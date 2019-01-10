1330285b297145b685f977c63b20fe090b7f75cc
Mark van der Loo <mark.vanderloo@gmail.com>
2019-01-10 13:18:54 +0100

    type

--- pkg/tests/testthat/test_median.R
+++ pkg/tests/testthat/test_median.R
@@ -7,6 +7,6 @@ test_that("correct medians are imputed",{
   iris2[1,1] <- iris2[1:3, 2] <- NA
   
   out <- simputation::impute_median(iris2, . ~ Species)
-  expect_equal(out[1:3,2]c(3.4,2.8,2.8))
+  expect_equal(out[1:3,2],c(3.4,2.8,2.8))
   
 })
