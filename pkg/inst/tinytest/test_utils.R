f5411c965ca74fd141d523089610308e4a84adb4
Karl Dunkle Werner <karldw@users.noreply.github.com>
2019-12-12 02:04:34 -0800

    Add deparse() replacement (#28)
    * Autoremove spaces
    
    * Add deparse() replacement
    
    Closes #27

--- pkg/inst/tinytest/test_utils.R
+++ pkg/inst/tinytest/test_utils.R
@@ -15,3 +15,8 @@ expect_error(impute_lm(haha, Sepal.Width ~ 1 | Species))
   expect_equal(sum(is.na(na.roughfix(a))),0)
   expect_equal(a,na.rpart(a))
 
+long_formula <- this_is_a_formula_with_long_variables ~
+  the_test_is_checking_if_deparse_will_return +
+  multiple_strings_or_not
+expect_equal(length(base::deparse(long_formula)), 2)
+expect_equal(length(simputation:::deparse(long_formula)), 1)
